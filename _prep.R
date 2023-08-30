7925edc915965011005f22e43c0505a9545a6b7f
Timo Thoms <timo@geeky.net>
2023-08-30 09:36:16 -0400

    browse (wip)

---
+++ _prep.R
@@ -0,0 +1,12 @@
+library(tidyverse)
+map(dir("_data"), function(file) {
+  read_csv(here::here("_data", file)) %>%
+    names() %>%
+    tibble(variable = .) %>%
+    mutate(table = str_replace(file, ".csv", ""))
+}) %>%
+  do.call(rbind, .) %>%
+  select(table, variable) %>%
+  arrange(table, variable) %>%
+  mutate(include_as = NA) %>%
+  write_csv(here::here("_data", "meta.csv"), na = "")
